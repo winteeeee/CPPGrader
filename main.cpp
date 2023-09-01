dc5a53ec5b1b38346cb07ec020e0ef4cbbb31224
winteeeee <b12e34e56@naver.com>
2023-09-02 00:37:15 +0900

    [main :: 파일 순회 구현]

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
+#include <filesystem>
 #include <iostream>
+#include <vector>
 using namespace std;
+namespace fs = filesystem;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<string> sources;
+
+    for (const fs::directory_entry &entry : fs::recursive_directory_iterator(fs::current_path())) {
+        if (entry.is_directory() && !sources.empty()) {
+            // 컴파일 및 실행
+            for (const string &source: sources) {
+                cout << source << endl;
+            }
+            cout << "================" << endl;
+            sources.clear();
+        } else {
+            string filename = entry.path().filename().string();
+            if (filename.find(".cpp") != string::npos) {
+                sources.push_back(filename);
+            }
+        }
+    }
+
+    // 컴파일 및 실행
+    for (const string &source: sources) {
+        cout << source << endl;
+    }
+    cout << "================" << endl;
+    sources.clear();
+
     return 0;
 }
