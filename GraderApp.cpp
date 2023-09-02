2b651d2faea2c479d31dd5d2361cc1a8b7fa2aa7
wintee <b12e34e56@naver.com>
2023-09-03 00:51:52 +0900

    [앱 :: 실행 동작 확인]

--- GraderApp.cpp
+++ GraderApp.cpp
@@ -1,12 +1,26 @@
 #include "GraderApp.h"
 
+vector<string> GraderApp::테스트케이스_로드() {
+    vector<string> 테스트케이스들;
+
+    for (const fs::directory_entry &엔트리 : fs::recursive_directory_iterator(fs::current_path())) {
+        string 경로 = 엔트리.path().string();
+
+        if (경로.find(".in") != string::npos) {
+            테스트케이스들.push_back(경로);
+        }
+    }
+
+    return 테스트케이스들;
+}
+
 void GraderApp::실행() {
     int 옵션;
     cout << "컴파일 옵션(0 : 일반, 1 : 각각 컴파일) : ";
     cin >> 옵션;
 
     vector<string> 소스코드들;
-    vector<string> 테스트케이스들;
+    vector<string> 테스트케이스들 = 테스트케이스_로드();
     auto 컴파일러 = CppCompiler(소스코드들, 테스트케이스들);
     string 현재_디렉토리;
 
--- GraderApp.h
+++ GraderApp.h
@@ -8,6 +8,9 @@ using namespace std;
 namespace fs = filesystem;
 
 class GraderApp {
+private:
+    vector<string> 테스트케이스_로드();
+
 public:
     void 실행();
 };
