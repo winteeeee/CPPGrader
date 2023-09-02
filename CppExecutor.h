8a6448cbbd883f749b3923ae6fe0546c4a492f2d
winteeeee <b12e34e56@naver.com>
2023-09-02 21:03:40 +0900

    [앱 :: 컴파일 로직 구현]

---
+++ CppExecutor.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+class CppExecutor {
+public:
+    void 실행(const string& 테스트케이스_경로);
+};
---
+++ GraderApp.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <iostream>
+#include "CppCompiler.h"
+using namespace std;
+namespace fs = filesystem;
+
+class GraderApp {
+public:
+    void 실행();
+};
